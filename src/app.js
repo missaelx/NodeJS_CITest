92667417a510916f6acb1b2cbbd765b190ab248a
mhernandez <mhernandez@idisc.com>
2020-11-04 13:32:10 -0700

    actualizacion de dependencias

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ app.use("/api", apiRoute);
 
 
 app.listen(config.PORT, config.BINDIND_IP, function() {
-    console.log("App listening on " + config.BINDIND_IP + " at " + config.PORT + "!");
+    console.log("App listening on " + config.BINDIND_IP + " at " + config.PORT + "!")
 })
